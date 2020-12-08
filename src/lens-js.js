e908010245f9d1874d29e183e8b21155a2bda05b
svinokot <svinokot@svinokot>
2020-12-08 17:42:54 +0300

    utils

--- src/lens-js.js
+++ src/lens-js.js
@@ -4,52 +4,6 @@
  * and open the template in the editor.
  */
 
-/**
- * Read-write mapper
- * @param {Function} getter
- * @param {Function} setter
- * @returns {Mapper}
- */
-export function Mapper(getter, setter) {
-	this.getter = getter;
-	this.setter = setter;
-}
-
-/**
- * Creating callback, which will be called only if current node would be changed
- * @param {Function} callback
- * @returns {Function}
- */
-export const getStrictCallback = (callback) => (e) => {
-	const { current } = e;
-	current && callback(e);
-}
-
-/**
- * Getting array of Lens from any node
- * @param {Lens} lens
- * @returns {Array<Lens>}
- */
-export const getArray = (lens) => {
-	const raw = lens.get();
-	return Object.keys(raw).map(k => lens.go(k));
-}
-
-/**
- * Create mappable fatory
- * @param {Mapper} mapper
- * @returns {Function}
- */
-export const getFactory = ({ getter, setter }) => (factory) => (key, parent) => {
-	const lens = factory(key, parent);
-	
-	return new Lens(
-		() => getter(lens.get()),
-		(value) => lens.set(setter(value, lens.get())),
-		parent
-	);
-}
-
 function AttachEvent(current, diffs) {
 	this.current = current;
 	this.diffs = diffs;
