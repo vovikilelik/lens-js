3cc9de1f5cc29a8984fa571bb098d24cb441067a
svinokot <svinokot@svinokot>
2021-04-26 00:29:25 +0300

    lens-js 1.5.0

--- src/lens-js.js
+++ src/lens-js.js
@@ -1,7 +1,7 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Lens
+ * version 1.5.x
+ * LGPLv3
  */
 
 export function AttachEvent(current, diffs) {
--- src/lens-utils.js
+++ src/lens-utils.js
@@ -1,9 +1,9 @@
 import {Lens} from './lens-js.js';
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Lens Utils
+ * version 1.5.x
+ * LGPLv3
  */
 
 /**
