c0af4fcd74e8cdc00b58cc94719c90c410f352ff
Your Name <you@example.com>
2024-03-22 21:32:36 +0300

    add store setter

--- src/store.js
+++ src/store.js
@@ -26,9 +26,9 @@ export class Store extends Lens {
 		return this._version || 0;
 	}
 
-	set(...args) {
+	set(value, ...args) {
 		this._version ? this._version++ : (this._version = 1);
-		super.set(...args);
+		super.set(typeof value === 'function' ? value(this.get()) : value, ...args);
 	}
 
 	go(key, instance = Store) {
